ff0eccf9f7a65e5fa41c6fc5a2fa98ea283be75f
Angel.Espinoza <angelo_19_94@hotmail.com>
2024-12-18 18:04:10 -0600

    Add project files.

---
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+StreamerDbContext dbContext = new();
+
+//await AddNewRecords();
+//QueryStreaming();
+//await QueryFilter();
+//await AddNewStreamerWithVideo();
+//await AddNewActorWithVideo();
+await AddNewDirectorWithVideo();
+
+Console.WriteLine("Presione una tecla para salir");
+Console.ReadKey();
+
+async Task AddNewDirectorWithVideo()
+{
+    var director = new Director { Nombre = "Lorenzo", Apellido = "Basteri", VideoId = 7 };
+    await dbContext.AddAsync(director);
+    await dbContext.SaveChangesAsync();
+}
+
+async Task AddNewActorWithVideo()
+{
+    var actor = new Actor { Nombre = "Brad", Apellido = "Pitt" };
+    await dbContext.AddAsync(actor);
+    await dbContext.SaveChangesAsync();
+
+    var videoActor = new VideoActor { ActorId = actor.Id, VideoId = 7 };
+
+    await dbContext.AddAsync(videoActor);
+    await dbContext.SaveChangesAsync();
+}
+async Task AddNewStreamerWithVideo()
+{
+    var pantalla = new Streamer { Nombre = "Pantalla" };
+    var pelicula = new Video { Nombre = "Hunger Games", Streamer = pantalla };
+
+    await dbContext.AddAsync(pelicula);
+    await dbContext.SaveChangesAsync(); 
+    
+}
+async Task QueryFilter()
+{
+    Console.WriteLine($"Ingrese una compañia de streaming:");
+    var streamingnombre = Console.ReadLine();
+
+    var streamers = await dbContext.Streamers.Where(x => x.Nombre == streamingnombre).ToListAsync();
+
+    foreach(var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    }
+}
+
+
+void QueryStreaming()
+{
+    var streamers = dbContext!.Streamers!.ToList();
+
+    foreach(var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id} - {streamer.Nombre}");
+    }
+}
+
+async Task AddNewRecords()
+{
+    Streamer streamer = new() { Nombre = "Disney", Url = "https://www.disney.com" };
+    dbContext!.Streamers!.Add(streamer);
+    await dbContext!.SaveChangesAsync();
+
+    var movies = new List<Video>
+    {
+        new Video
+        {
+            Nombre = "La Cenicienta",
+            StreamerId = streamer.Id
+        },
+        new Video
+        {
+            Nombre = "101 Dalmatas",
+            StreamerId = streamer.Id
+        },
+        new Video
+        {
+            Nombre = "El Jorobado de Notredame",
+            StreamerId = streamer.Id
+        }
+    };
+
+
+    await dbContext.AddRangeAsync(movies);
+    await dbContext.SaveChangesAsync();
+}
---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-K394INF; Initial Catalog=Streamer; User Id=sa; Password=Notiene12345")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Video>()
+                .HasMany(p => p.Actores)
+                .WithMany(t => t.Videos)
+                .UsingEntity<VideoActor>(
+                    pt => pt.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
